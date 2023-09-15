7f68ef3654a95933bdfe9ce049b8a72929cf167f
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-09-15 09:28:46 +0500

    cast

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server Error";
 
+  /// cast error
+  if (err.name === "CastError") {
+    const message = `resource not found . Invalid: ${err.path}`;
+    err = new ErrorHander(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
