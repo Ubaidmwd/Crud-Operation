7d47b69fd302c701cf203dfb90d30cc01a17bb3e
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-09-15 09:21:12 +0500

    Uncaught & unHandleReject

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,29 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const dbconnection = require("./config/database");
 
+////////// Uncaught ////////
+process.on("uncaughtException",(err)=>{
+  console.log(`Error: ${err.message}`)
+  console.log("The server is shutting down due to the uncaughtException Error")
+  process.exit(1)
+
+})
+
+
 dotenv.config({ path: "backend/config/config.env" });
+
+///////Database////////
 dbconnection();
-app.listen(process.env.PORT, () => {
+
+const server=app.listen(process.env.PORT, () => {
   console.log(`server is working on http://localhost ${process.env.PORT}`);
 });
+
+///// Unhandle Promis Rejection////////
+process.on("unhandledRejection",(err)=>{
+  console.log(`Error: ${err.message}`)
+  console.log("The server is shutting down due to the Unhandle promise rejection")
+  server.close(()=>{
+    process.exit(1)
+  })
+})
